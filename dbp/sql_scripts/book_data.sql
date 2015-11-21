405af6ae53c43dcb03280f290ae4a00efdcd97eb
marcus <marcustan03@yahoo.com.sg>
2015-11-21 18:38:06 +0800

    Populated db with books data and organized sql scripts

---
+++ dbp/sql_scripts/book_data.sql
@@ -0,0 +1,21 @@
+insert into books values ('978-0345803481', 'Fifty Shades of Grey: Book One of the Fifty Shades Trilogy (Fifty Shades of Grey Series)', 'E L James', 'Vintage Books; 1 edition (April 3, 2012)',  2012, 48, 9.56, 'Softcover', 'fifty shades of grey', 'Romance', 'http://ecx.images-amazon.com/images/I/51RhsdrKNHL.jpg');
+
+insert into books values ('978-0545663267', 'Mockingjay (The Hunger Games)', 'Suzanne Collins', 'Scholastic Press; 1st edition (February 25, 2014)',  2014, 96, 7.36, 'Softcover', 'hunger games katniss everdeen', 'Adventure, War, Science Fiction, Thriller,', 'http://ecx.images-amazon.com/images/I/51ywQQjkppL.jpg');
+
+insert into books values ('978-0142424179', 'The Fault in Our Stars', 'John Green', 'Penguin Books; Reprint edition (April 8, 2014)',  2014, 64, 10.60, 'Hardcover', 'fault in our stars', 'Romance', 'http://ecx.images-amazon.com/images/I/51VlKD1aucL.jpg');
+
+insert into books values ('978-0142415436', 'If I Stay', 'Gayle Forman', 'Speak; Reprint edition (April 6, 2010)',  2010, 53, 11.42, 'Hardcover', 'stay teen fiction', 'Teen Fiction', 'http://ecx.images-amazon.com/images/I/41rEe4pc3yL._AC_UL115_.jpg');
+
+insert into books values ('978-0253009081', 'The Imjin and Kapyong Battles, Korea, 1951 (Twentieth-Century Battles)', 'Paul MacKenzie', 'Indiana University Press (March 12, 2013)',  2013, 121, 18.27, 'Hardcover', 'imjin war history', 'Military History', 'http://ecx.images-amazon.com/images/I/71vqiI8AVgL.jpg');
+
+insert into books values ('978-0321884497', 'Database Design for Mere Mortals: A Hands-On Guide to Relational Database Design (3rd Edition)', 'Michael J. Hernandez', 'Addison-Wesley Professional; 3 edition (February 24, 2013)',  2013, 46, 22.17, 'Softcover', 'database design', 'Computer Science Textbook', 'http://ecx.images-amazon.com/images/I/51NnqXILVjL.jpg');
+
+insert into books values ('978-1435457515', 'The Language of SQL: How to Access Data in Relational Databases', 'Larry Rockoff', 'Cengage Learning PTR; 1 edition (June 3, 2010)',  2010, 19, 11.59, 'Softcover', 'sql language relational database', 'Computer Science Textbook', 'http://ecx.images-amazon.com/images/I/41t57zHR%2BXL.jpg');
+
+insert into books values ('978-0345544940', 'Leaving Time', 'Jodi Picoult', 'Ballantine Books; Reprint edition (April 28, 2015)',  2015, 16, 10.81, 'Softcover', 'leaving time jodi picoult', 'Mystery, Thriller, Suspense', 'http://ecx.images-amazon.com/images/I/51t7D9BYEAL.jpg');
+
+insert into books values ('978-1118380987', 'Networking All-in-One For Dummies', 'Doug Lowe', 'For Dummies; 5 edition (November 28, 2012)',  2012, 33, 27.11, 'Softcover', 'networking dummies all-in-one', 'Computer Science Textbook', 'http://ecx.images-amazon.com/images/I/51xykVSI7wL.jpg');
+
+insert into books values ('978-0470450604', 'TCP / IP For Dummies', 'Candace Leiden', 'For Dummies; 6 edition (August 11, 2009)',  2009, 28, 24.26, 'Softcover', 'tcp ip dummies network', 'Computer Science Textbook', 'http://ecx.images-amazon.com/images/I/51UH47L1tJL.jpg');
+
+insert into books values ('978-0262029445', 'Fundamentals of Machine Learning for Predictive Data Analytics: Algorithms, Worked Examples, and Case Studies', 'John D. Kelleher, Brian Mac Namee', 'The MIT Press (July 24, 2015)',  2015, 24, 63.00, 'Hardcover', 'machine learning algorithm data analysis prediction', 'Computer Science Textbook', 'http://ecx.images-amazon.com/images/I/51%2ByIgLek2L.jpg');
