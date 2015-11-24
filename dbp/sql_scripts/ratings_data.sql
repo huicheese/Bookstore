6762fc7d5fc2538aecc2dad839be6ebfb01e54e1
marcus <marcustan03@yahoo.com.sg>
2015-11-24 23:01:13 +0800

    populated more entries

--- dbp/sql_scripts/ratings_data.sql
+++ dbp/sql_scripts/ratings_data.sql
@@ -11,3 +11,59 @@ insert into ratings values ('978-0262029445', 'lololol', 'mhatib', 0);
 insert into ratings values ('978-0262029445', 'lololol', 'dabinlab', 0);
 
 insert into ratings values ('978-1118380987', 'dabinlab', 'mhatib', 1);
+
+insert into ratings values ('978-1435457515', 'cy', 'mhatib', 1);
+insert into ratings values ('978-1435457515', 'cy', 'lololol', 1);
+insert into ratings values ('978-1435457515', 'cy', 'dabinlab', 2);
+insert into ratings values ('978-1435457515', 'cy', '1234', 0);
+
+insert into ratings values ('978-1118380987', 'dabinlab', 'huisquare', 2);
+insert into ratings values ('978-1118380987', 'dabinlab', 'lololol', 1);
+insert into ratings values ('978-1118380987', 'dabinlab', 'cy', 2);
+insert into ratings values ('978-1118380987', 'dabinlab', '1234', 0);
+
+insert into ratings values ('978-0545663267', 'lololol', 'mhatib', 1);
+insert into ratings values ('978-0545663267', 'lololol', 'huisquare', 1);
+insert into ratings values ('978-0545663267', 'lololol', 'dabinlab', 1);
+insert into ratings values ('978-0545663267', 'lololol', '1234', 0);
+
+insert into ratings values ('978-0321884497', 'huisquare', 'mhatib', 2);
+insert into ratings values ('978-0321884497', 'huisquare', 'lololol', 1);
+insert into ratings values ('978-0321884497', 'huisquare', 'dabinlab', 2);
+insert into ratings values ('978-0321884497', 'huisquare', '1234', 0);
+insert into ratings values ('978-0321884497', 'huisquare', 'cy', 1);
+
+insert into ratings values ('978-0253009081', 'mhatib', 'huisquare', 1);
+insert into ratings values ('978-0253009081', 'mhatib', 'lololol', 0);
+insert into ratings values ('978-0253009081', 'mhatib', 'dabinlab', 1);
+insert into ratings values ('978-0253009081', 'mhatib', '1234', 1);
+insert into ratings values ('978-0253009081', 'mhatib', 'cy', 1);
+
+
+
+insert into ratings values ('978-0470450604', '1234', 'mhatib', 1);
+insert into ratings values ('978-0470450604', '1234', 'lololol', 1);
+insert into ratings values ('978-0470450604', '1234', 'dabinlab', 2);
+insert into ratings values ('978-0470450604', '1234', 'huisquare', 1);
+
+insert into ratings values ('978-0345803481', 'mhatib', 'huisquare', 1);
+insert into ratings values ('978-0345803481', 'mhatib', 'lololol', 1);
+insert into ratings values ('978-0345803481', 'mhatib', 'cy', 1);
+insert into ratings values ('978-0345803481', 'mhatib', '1234', 0);
+
+insert into ratings values ('978-0321884497', 'lololol', 'mhatib', 0);
+insert into ratings values ('978-0321884497', 'lololol', 'huisquare', 1);
+insert into ratings values ('978-0321884497', 'lololol', 'dabinlab', 1);
+insert into ratings values ('978-0321884497', 'lololol', '1234', 0);
+
+insert into ratings values ('978-0142415436', 'huisquare', 'mhatib', 2);
+insert into ratings values ('978-0142415436', 'huisquare', 'lololol', 1);
+insert into ratings values ('978-0142415436', 'huisquare', 'dabinlab', 2);
+insert into ratings values ('978-0142415436', 'huisquare', '1234', 2);
+insert into ratings values ('978-0142415436', 'huisquare', 'cy', 1);
+
+insert into ratings values ('978-0470450604', 'mhatib', 'huisquare', 1);
+insert into ratings values ('978-0470450604', 'mhatib', 'lololol', 2);
+insert into ratings values ('978-0470450604', 'mhatib', 'dabinlab', 1);
+insert into ratings values ('978-0470450604', 'mhatib', '1234', 2);
+insert into ratings values ('978-0470450604', 'mhatib', 'cy', 1);
