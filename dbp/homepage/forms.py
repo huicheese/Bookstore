29f34645646c1dbbf834d64d1e86f39dd4de7a75
Huicheese <huihui.tiang@gmail.com>
2015-12-12 18:16:23 +0800

    modified the UI

--- dbp/homepage/forms.py
+++ dbp/homepage/forms.py
@@ -10,7 +10,7 @@ class loginform(forms.Form):
     pw = forms.CharField(label='Password', max_length=10, widget=forms.PasswordInput)
 
 class advsearchform(forms.Form):
-    authors = forms.CharField(label='Authors')
+    authors = forms.CharField(label='Authors', widget=forms.TextInput(attrs={'class' : 'form-horizontal'}))
     publisher = forms.CharField(label='Publisher')
     title = forms.CharField(label='Title')
     subject = forms.CharField(label='Subject')
